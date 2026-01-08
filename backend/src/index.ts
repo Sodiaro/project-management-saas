1de6d1d1e28a58f62b160a428ff1766c47bb5995
Sodiaro <semiu.sodiq123@gmail.com>
2026-01-09 00:39:39 +0100

    feat: implement Google Oauth Endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,10 @@ import { errorHandler } from "./middlewares/errorHandler.middleware";
 import { BadRequestException } from "./utils/appError";
 import { ErrorCodeEnum } from "./enums/error-code.enum";
 
+import "./config/passport.config";
+import passport from "passport";
+import authRoutes from "./routes/auth.route";
+
 const app = express();
 const BASE_PATH = config.BASE_PATH;
 
@@ -28,6 +32,10 @@ app.use(
     sameSite: "lax",
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use(
   cors({
@@ -48,6 +56,8 @@ app.get(
     });
   })
 );
+
+app.use(`${BASE_PATH}/auth`, authRoutes);
 
 app.use(errorHandler);
 
