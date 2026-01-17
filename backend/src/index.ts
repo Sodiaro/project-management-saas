575864def44b381f9c95e535f29356d09e3ce9b5
Sodiaro <semiu.sodiq123@gmail.com>
2026-01-17 02:16:31 +0100

    feat: add project endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,8 @@ import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
 import workspaceRoutes from "./routes/workspace.route";
+import projectRoutes from "./routes/project.route";
+import memberRoutes from "./routes/member.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -62,6 +64,8 @@ app.get(
 app.use(`${BASE_PATH}/auth`, authRoutes);
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
 
 app.use(errorHandler);
 
